25efa7db666c9f061d1fd6cdaf97f9d18490ec8f
Felipe Olivares <68532280+FelipeFbh@users.noreply.github.com>
2023-05-31 21:47:51 -0400

    update

--- Tarea2_Equiprobable.cpp
+++ Tarea2_Equiprobable.cpp
@@ -3,9 +3,33 @@
 #include <math.h>   // Para pow (potencias)
 #include <algorithm> //Para lograr
 #include <random>   // garantizar que el arreglo sea aleatorio
-
 using namespace std;
 
-void main(){
-    
+void main_splay(int n){
+    int *A = new int[n];
+    //Se llena el arreglo con los numeros del 1 al n
+    iota(A, A + n, 1);
+    //Se permuta el arreglo de manera aleatoria
+    shuffle(A, A + n, mt19937{random_device{}()});    
+    free(A);
+}
+
+void main_rbtree(int n){
+    int *A = new int[n];
+    //Se llena el arreglo con los numeros del 1 al n
+    iota(A, A + n, 1);
+    //Se permuta el arreglo de manera aleatoria
+    shuffle(A, A + n, mt19937{random_device{}()});  
+    free(A);
+}
+
+int main(){
+    //Testeamos 10 veces para el n dado
+    for(int i=0; i<5; i++){
+        for (int j=16; j<25; j++){
+            main_splay(j);
+            main_rbtree(j);
+        }
+    }
+            
 }
