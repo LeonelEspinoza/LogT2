30f023d3e581b2dcbac37238f1b11a4636fbd7d9
salocinc <nicolascalbucuravillalobos@gmail.com>
2023-05-31 23:39:58 -0400

    implementation of main_splay

--- Tarea2_Equiprobable.cpp
+++ Tarea2_Equiprobable.cpp
@@ -8,11 +8,61 @@
 
 using namespace std;
 
-void main_splay(int n){
+void main_splay(int exp_n, int res[], int j){
+    int m = pow(2,28);
+    int n = pow(2, exp_n);
+    int *M = new int[m];
+    int repeticiones_n = pow(2, 28 - exp_n);
     
+    //Se llena el arreglo con los numeros del 1 al n, repitiendose 2^28/n veces
+    int puntero = 0;
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<repeticiones_n; j++){
+            M[puntero++] = i;
+        }
+    }
+    //Se permuta el arreglo M de manera aleatoria
+    shuffle(M, M + m, mt19937{random_device{}()});
+
+    //Se crea el arbol
+    node *root = insert(NULL, 1);
+    for(int i=2; i<=n; i++){
+        root=insert(root, i);
+    }
+    //Se inicia el cronometro
+    auto inicio = chrono::high_resolution_clock::now();
+
+    //Se busca cada elemento del arreglo M en el arbol
+    for(int i=0; i<m; i++){
+        search(root, M[i]);
+    }
+
+    //Se finaliza el cronometro
+    auto fin = chrono::high_resolution_clock::now();
+
+    //Se calcula el tiempo transcurrido
+    auto duracion = chrono::duration_cast<chrono::milliseconds>(fin - inicio).count();
+
+    //Se guarda el resultado en el arreglo de resultados
+    res[j] = int(duracion);
+
+    //Se abre el archivo donde se guardarán los tiempos
+    FILE *f = fopen("resultados.txt", "a"); //append
+
+    //Se escribe el numero del test
+    fprintf(f, "Test %d:\n", j+1);
+
+    //Se escribe el tiempo en el archivo
+    fprintf(f, "La búsqueda tardó %ld milisegundos en ejecutarse.\n\n", duracion);
+
+    //Se cierra el archivo
+    fclose(f);
+
+    //Se libera la memoria
+    delete[] M;
 }
 
-void main_rbtree(int n){
+void main_rbtree(int exp_n){
     int *A = new int[n];
     //Se llena el arreglo con los numeros del 1 al n
     iota(A, A + n, 1);
